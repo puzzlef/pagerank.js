a790840d4f452e7840afa7ce7e92b30f3feabe6f
Subhajit Sahu <wolfram77@gmail.com>
2021-05-29 13:21:19 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+exports.mtx = require('./mtx');
+exports.DiGraph = require('./DiGraph');
+exports.pagerankPush = require('./pagerankPush');
+exports.pagerankPushAdjust = require('./pagerankPushAdjust');
---
+++ src/mtx.js
@@ -0,0 +1,41 @@
+const DiGraph = require('./DiGraph');
+const file = require('./file');
+
+
+
+
+function readTo(a, pth) {
+  var d  = file.read(pth);
+  var ls = d.split('\n'), l = 0;
+
+  // read header
+  while (true) {
+    var ln = ls[l++];
+    if (!ln.startsWith('%')) break;
+    if (!ln.startsWith('%%')) continue;
+    var [h0, h1, h2, h3, h4] = ln.split(/\s+/g);
+  }
+  if (h1!="matrix" || h2!="coordinate") return;
+  var sym = h4=="symmetric" || h4=="skew-symmetric";
+
+  // read rows, cols, size
+  var [r, c, sz] = ln.split(/\s+/g).map(parseFloat);
+  var n = Math.max(r, c);
+  for (var u=1; u<=n; u++)
+    a.addVertex(u);
+
+  // read edges (from, to)
+  while (l < ls.length) {
+    ln = ls[l++];
+    var [u, v] = ln.split(/\s+/g).map(parseFloat);
+    if (isNaN(u) || isNaN(v)) break;
+    a.addEdge(u, v);
+    if (sym) a.addEdge(v, u);
+  }
+}
+
+function read(pth) {
+  var a = new DiGraph(); readTo(a, pth);
+  return a;
+}
+module.exports = {readTo, read};
